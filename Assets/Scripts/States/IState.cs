ddecaa96cbf062dc5fdfb250d7c75357265b4bcf
lushdog <lushdog@ef872e14-358b-4bfc-814a-65c9ae95810a>
2014-07-28 22:02:02 +0000

    Started basic state machine stuff.
    git-svn-id: https://lulz.unfuddle.com/svn/lulz_unitybjj@3 ef872e14-358b-4bfc-814a-65c9ae95810a

---
+++ Assets/Scripts/States/IState.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public interface IState  
+{
+    List<IState> SubStates 
+    {
+        get;
+        set;
+    } 
+
+    void OnEnterState(); 
+    void InState();
+    void ExitState();
+}
+
+public class IdleState : IState
+{
+    public void OnEnterState ()
+    {
+        throw new System.NotImplementedException ();
+    }
+
+    public void InState ()
+    {
+        throw new System.NotImplementedException ();
+    }
+
+    public void ExitState ()
+    {
+        throw new System.NotImplementedException ();
+    }
+}
+
+public class GrabbingState : IState
+{
+    public void OnEnterState ()
+    {
+        throw new System.NotImplementedException ();
+    }
+
+    public void InState ()
+    {
+        throw new System.NotImplementedException ();
+    }
+
+    public void ExitState ()
+    {
+        throw new System.NotImplementedException ();
+    }
+}
